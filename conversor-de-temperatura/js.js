8f305f1f9174f715e0159dc3f510e945b1554565
alekleao <alecs.sousa44@gmail.com>
2021-09-13 23:52:09 -0300

    1 dia imersão dev
    #alura

---
+++ conversor-de-temperatura/js.js
@@ -0,0 +1,15 @@
+// var serve para criar uma variavel
+// var nome = "variavel"
+// console.log("isso é " + nome)
+
+var notaDoPrimeiroBimestre = 1
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 6
+var notaDoQuartoBimestre = 9
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+console.log(notaFinal)
+
+var notaReal = notaFinal.toFixed(1)
+
+console.log(notaReal)
